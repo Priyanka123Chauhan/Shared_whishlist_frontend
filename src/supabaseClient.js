1aa8adfc47cacd9d57732a25c7d364e4de8dad6c
Priyanka123Chauhan <priyankachauhan.career@gmail.com>
2025-07-07 13:16:08 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,5 +4,6 @@ import { createClient } from "@supabase/supabase-js";
 
 
 const SUPABASE_URL = "https://ehgnkwmrtbfwzzebvvqm.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoZ25rd21ydGJmd3p6ZWJ2dnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3ODQ2MTEsImV4cCI6MjA2NzM2MDYxMX0.8vrGHGe6pX82dnzYGkAstfmKpYyeXcHcizA0dbkTwPU"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoZ25rd21ydGJmd3p6ZWJ2dnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3ODQ2MTEsImV4cCI6MjA2NzM2MDYxMX0.8vrGHGe6pX82dnzYGkAstfmKpYyeXcHcizA0dbkTwPU";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+    

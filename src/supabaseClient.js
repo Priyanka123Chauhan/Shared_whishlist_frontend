edff5489c757506caf583c739210ad4ff83abccc
Priyanka123Chauhan <priyankachauhan.career@gmail.com>
2025-07-07 23:31:53 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -7,5 +7,5 @@ const SUPABASE_URL = "https://ehgnkwmrtbfwzzebvvqm.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoZ25rd21ydGJmd3p6ZWJ2dnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3ODQ2MTEsImV4cCI6MjA2NzM2MDYxMX0.8vrGHGe6pX82dnzYGkAstfmKpYyeXcHcizA0dbkTwPU";
 const DATABASE_URL = "postgresql://postgres:Pdc!!123@db.ehgnkwmrtbfwzzebvvqm.supabase.co:5432/postgres"
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, DATABASE_URL);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
     
